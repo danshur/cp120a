a1eb2bfec1ad45e0e710f20bc252b5adc69b6d82
Dan Shur <danshur@gmail.com>
2016-02-13 10:21:05 -0800

    first checkin

---
+++ LambdaExercise.java
@@ -0,0 +1,43 @@
+package cp120a.dshur.hw4;
+
+import java.util.Arrays;
+
+public class LambdaExercise {
+
+	public static void main(String[] args) {
+		      String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars", 
+		              "Jupiter", "Saturn", "Uranus", "Neptune" };
+		        System.out.println("Original planets array");
+		        System.out.println(Arrays.toString(planets));
+		        System.out.println("Sorted in dictionary order (ascending):");
+		        Arrays.sort(planets);
+		        System.out.println(Arrays.toString(planets));
+
+		        // lambda expression , dictionary order (descending)
+		        System.out.println("Sorted in dictionary order (descending)");
+		        Arrays.sort(planets, ??);
+		        System.out.println(Arrays.toString(planets));
+
+		        // lambda expression , short to long
+		        // hint the expression can be found in the LambdaTest.java from the book.
+		        System.out.println("Sorted by length (ascending):");
+		        Arrays.sort(planets, ??);
+		        System.out.println(Arrays.toString(planets));
+
+		        // lambda expression , long to short
+		        System.out.println("Sorted by length (descending):");
+		        Arrays.sort(planets, ??);
+		        System.out.println(Arrays.toString(planets));
+		        
+		        // lambda expression , reverse the name , then sort in ascending order
+		        // hint use  new StringBuilder(first).reverse().toString() to reverse the string
+		        System.out.println("Sorted in dictionary order of the reversed name (ascending)");
+		        Arrays.sort(planets, ??);
+		        System.out.println(Arrays.toString(planets));
+
+		        // lambda expression , reverse the name , then sort in descending order
+		        System.out.println("Sorted in dictionary order of the reversed name (descending)");
+		        Arrays.sort(planets, ??);
+		        System.out.println(Arrays.toString(planets));
+		}
+}
