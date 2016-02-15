2881b6a301001bb1ddc604d1f8321de3bfcff322
Dan Shur <danshur@gmail.com>
2016-02-15 07:52:03 -0800

    updated

--- LambdaExercise.java
+++ LambdaExercise.java
@@ -1,6 +1,7 @@
 package cp120a.dshur.hw4;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class LambdaExercise {
 
@@ -15,29 +16,30 @@ public class LambdaExercise {
 
 		        // lambda expression , dictionary order (descending)
 		        System.out.println("Sorted in dictionary order (descending)");
-		        Arrays.sort(planets, ??);
+		        //Arrays.sort(planets, Collections.reverseOrder());
+		        Arrays.sort(planets, (a, b) -> b.compareTo(a));
 		        System.out.println(Arrays.toString(planets));
 
 		        // lambda expression , short to long
 		        // hint the expression can be found in the LambdaTest.java from the book.
 		        System.out.println("Sorted by length (ascending):");
-		        Arrays.sort(planets, ??);
+		        Arrays.sort(planets, (a, b) -> (a.length() - b.length()));
 		        System.out.println(Arrays.toString(planets));
 
 		        // lambda expression , long to short
 		        System.out.println("Sorted by length (descending):");
-		        Arrays.sort(planets, ??);
+		        Arrays.sort(planets, (a, b) -> b.length() - a.length());
 		        System.out.println(Arrays.toString(planets));
 		        
 		        // lambda expression , reverse the name , then sort in ascending order
 		        // hint use  new StringBuilder(first).reverse().toString() to reverse the string
 		        System.out.println("Sorted in dictionary order of the reversed name (ascending)");
-		        Arrays.sort(planets, ??);
+		        //Arrays.sort(planets, new StringBuilder(a).reverse().toString(), (a), b) -> a.compareTo(b)) ;
 		        System.out.println(Arrays.toString(planets));
 
 		        // lambda expression , reverse the name , then sort in descending order
-		        System.out.println("Sorted in dictionary order of the reversed name (descending)");
-		        Arrays.sort(planets, ??);
-		        System.out.println(Arrays.toString(planets));
+		        //System.out.println("Sorted in dictionary order of the reversed name (descending)");
+		        //Arrays.sort(planets, ??);
+		        //System.out.println(Arrays.toString(planets));
 		}
 }
